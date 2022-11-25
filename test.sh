e294bbc3d459619b38634e4e1ba69f689051473c
aliyou <aliyou@aliyou.com>
2022-11-25 22:30:41 +0100

    update12 test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ chmod +x myfile.sh
 echo "hello depuis myfile" > myfile
 git add .
 git commit -m "add myfile"
-git remote add origin23 git@github.com:Aliyoub/testsjenkins.git
+git remote add origin24 git@github.com:Aliyoub/testsjenkins.git
+unset SSH_ASKPASS
 git push origin HEAD:main
 ./myfile.sh

dfa44efc0baffb17ecb5669b82410e6d53ce75e4
aliyou <aliyou@aliyou.com>
2022-11-26 02:03:50 +0100

    update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ chmod +a myfile2.sh
 echo "hello depuis myfile2" > myfile2.sh
 git add .
 git commit -m "add myfile2.sh"
-git remote add aliyou2 git@github.com:Aliyoub/testsjenkins.git
-git push --set-upstream alyou2 temp
+git remote add aliyou3 git@github.com:Aliyoub/testsjenkins.git
+git push --set-upstream aliyou3 temp
 sh ./myfile2.sh
